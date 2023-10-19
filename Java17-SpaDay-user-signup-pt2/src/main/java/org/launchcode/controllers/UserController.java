4905feded007e1cc0b9d9ce1dbcfba5fcc1fef4c
AnidaMalkic <malkic.anida@gmail.com>
2023-10-18 22:51:52 -0500

    first commit

---
+++ Java17-SpaDay-user-signup-pt2/src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,36 @@
+package org.launchcode.controllers;
+
+import jakarta.validation.Valid;
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
+        return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+
+        model.addAttribute("verify", verify);
+
+        if (user.getPassword().equals(verify) && errors.hasErrors()) {
+            return "user/index";
+        }
+        else {
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+
+    }
+
+
+}
